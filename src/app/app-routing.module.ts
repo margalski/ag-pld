bc54d375b77f370cf5f1fe6fe1896d4d3867c9b7
Tomasz <tmargalski@pgs-soft.com>
2018-01-04 11:15:15 +0100

    split layout per components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules, NoPreloading } from '@angular/router';
-
-// import { PreloadModulesStrategy } from './core/strategies/preload-modules.strategy';
-
-const app_routes: Routes = [
-  { path: '', pathMatch:'full', redirectTo: '/products' }, 
-  { path: '**', pathMatch:'full', redirectTo: '/products' }  
-];
-
-@NgModule({
-  imports: [ RouterModule.forRoot(app_routes, { preloadingStrategy: PreloadAllModules }) ],
-  exports: [ RouterModule ]
-})
+ 
+@NgModule({ })
 
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ProductComponent }  from './product/product.component';
 import { ProductModule } from './product/product.module';
 // import { CoreModule }   from './core/core.module';
 // import { SharedModule }   from './shared/shared.module';
@@ -14,7 +15,7 @@ import { ProductModule } from './product/product.module';
     ProductModule,
     AppRoutingModule,
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
